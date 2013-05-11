05977b47b37254fe475f03fddcba1651ba185ad9
Stephen Kockentiedt <Stephen@Kockentiedt.name>
2013-05-11 12:15:51 +0200

    Converted tags to be full-fledged components.

--- src/simplecs/tags.clj
+++ src/simplecs/tags.clj
@@ -1,25 +1,26 @@
 (ns simplecs.tags
-  (:require [simplecs.core :refer (defcomponent entities-with-component get-component)]))
+  (:require [simplecs.core :as sc]))
 
-(defcomponent tags [& tags]
-  :tags (set tags))
+(defn tag
+  "Instead of defining a component '(defcomponent foo [])' and
+   adding '(foo)' to an entity, you can just add '(tag :foo)'
+   to the entity. There MUST NOT exist a component with the
+   same name as the tag keyword."
+  [tag]
+  {:pre [(keyword? tag)]}
+  {:name tag})
 
-(defn get-tags
-  "Returns a set of all tags for the given entity id."
-  [ces entity-id]
-  (-> ces
-      (get-component entity-id :tags)
-      :tags
-      (or #{})))
+(sc/defcomponent tag-to-entity [& paths]
+  :paths paths)
 
-(defn entities-with-tag
-  "Returns a set of all entities which contain the given tag."
-  [ces tag]
-  (->> ces
-      (#(entities-with-component % :tags))
-      (filter (fn [entity]
-                (-> entity
-                    (#(get-component ces % :tags))
-                    :tags
-                    (get tag))))
-       set))
+(sc/defcomponentsystem tag-to-entity-converter :tag-to-entity
+  []
+  [ces entity component]
+  (let [converted-ces (reduce (fn [ces path]
+                                (sc/update-entity ces
+                                                  entity
+                                                  path
+                                                  #(first (sc/entities-with-component ces %))))
+                              ces
+                              (:paths component))]
+    (sc/remove-component converted-ces entity :tag-to-entity)))
